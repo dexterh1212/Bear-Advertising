f58efe33cfbcaf24c9a0751d8fef998246ae6910
dexterh1212 <dexterhowells19@outlook.com>
2026-09-20 16:46:00 +0100

    Change bot activity from 'Watching' to 'Custom'

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Watching", // required by Discord API, not shown in the client
-        state: "Watching",     // this is what people actually see
+        name: "Custom", // required by Discord API, not shown in the client
+        state: "Custom",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
